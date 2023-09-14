0718f7a3dbdd2a6319f744a8bcbda1a9de8b7975
adhithiyan <adhithiyan.work@gmail.com>
2023-09-14 18:37:25 +0530

    add: routing to cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,17 @@
-import "./css/normalize.css"
-import "./css/header.css"
-import "./css/cards.css"
-import Header from "./main-page-components/Header.jsx"
-import Cards from "./main-page-components/Cards.jsx"
+import MainPage from "./MainPage.jsx"
+import CartPage from "./CartPage.jsx"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
 
-//details of all the items
-const details = [
-    {
-        id:1,
-        name: "Shirt 1",
-        price: "$",
-        img: "/images/shirt-1.png"
-    },
-    {
-        id:2,
-        name: "Shirt 2",
-        price: "$",
-        img: "/images/shirt-2.png"
-    },
-    {
-        id:3,
-        name: "Shirt 3",
-        price: "$",
-        img: "/images/shirt-3.png"
-    },
-    {
-        id:4,
-        name: "Shirt 4",
-        price: "$",
-        img: "/images/shirt-4.png"
-    },
-    {
-        id:5,
-        name: "extra 1",
-        price: "$",
-        img: "/images/extras-1.png"
-    },
-    {
-        id:6,
-        name: "extra 2",
-        price: "$",
-        img: "/images/extras-2.png"
-    },
-    {
-        id:7,
-        name: "extra 3",
-        price: "$",
-        img: "/images/extras-3.png"
-    },
-    {
-        id:8,
-        name: "extra 4",
-        price: "$",
-        img: "/images/extras-4.png"
-    },
-]
+
 
 function App() {
     return (
-        <>
-            <Header/>
-            <div class="products" >Our Products</div>
-            <div className="main-card">
-                {details.map(i => (
-                    <Cards key={i.id} {...i}/> 
-                ))}
-            </div>
-
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route index element= {<MainPage/>}/>
+                <Route path="/cart" element = {<CartPage/>} />
+            </Routes>
+        </BrowserRouter> 
     )
 }
 
---
+++ src/CartPage.jsx
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom"
+function InfoPage(){
+    const values = useLocation().state;
+    return(
+        <>
+            <div>asdasd</div>
+        </>
+    ) 
+}
+
+export default InfoPage
