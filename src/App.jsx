a88a9b79014a4f32ed8a5be6ee3a610ece6f8051
adhithiyan <adhithiyan.work@gmail.com>
2023-09-14 17:20:39 +0530

    add: more styling to cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
 import "./css/normalize.css"
 import "./css/header.css"
+import "./css/cards.css"
 import Header from "./main-page-components/Header.jsx"
+import Cards from "./main-page-components/Cards.jsx"
+
+//details of all the items
+const details = [
+    {
+        id:1,
+        name: "Shirt 1",
+        price: "$",
+        img: "/images/shirt-1.png"
+    },
+    {
+        id:2,
+        name: "Shirt 2",
+        price: "$",
+        img: "/images/shirt-2.png"
+    },
+    {
+        id:3,
+        name: "Shirt 3",
+        price: "$",
+        img: "/images/shirt-3.png"
+    },
+    {
+        id:4,
+        name: "Shirt 4",
+        price: "$",
+        img: "/images/shirt-4.png"
+    },
+    {
+        id:5,
+        name: "extra 1",
+        price: "$",
+        img: "/images/extras-1.png"
+    },
+    {
+        id:6,
+        name: "extra 2",
+        price: "$",
+        img: "/images/extras-2.png"
+    },
+    {
+        id:7,
+        name: "extra 3",
+        price: "$",
+        img: "/images/extras-3.png"
+    },
+    {
+        id:8,
+        name: "extra 4",
+        price: "$",
+        img: "/images/extras-4.png"
+    },
+]
+
 function App() {
     return (
         <>
             <Header/>
+            <div class="products" >Our Products</div>
+            <div className="main-card">
+                {details.map(i => (
+                    <Cards key={i.id} {...i}/> 
+                ))}
+            </div>
+
         </>
     )
 }
--- src/main-page-components/Cards.jsx
+++ src/main-page-components/Cards.jsx
@@ -1,8 +1,12 @@
-function Cards(){
+import "../css/cards.css"
+
+function Cards(props){
     return (
-        <>
-            
-        </>
+        <div class="cards" >
+            <img src={props.img} alt="" />
+            <div>{props.name}</div>
+            <div>{props.price}</div>
+        </div>
     )
 }
 
