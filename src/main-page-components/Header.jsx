8ba0f1584683ad4165e5a1adc8b39fcaff94f36b
adhithiyan <adhithiyan.work@gmail.com>
2023-09-16 06:49:21 +0530

    add: import 2 images for static asset handling

--- src/main-page-components/Header.jsx
+++ src/main-page-components/Header.jsx
@@ -1,13 +1,15 @@
+import logo from "/images/logo.png"
+import pfp from "/images/pfp.jpg"
 function Header(props){
     return(
         <>
             <div className="header">
                 <div className="sub-header">
-                    <a href="https://www.dscommunity.in/" target="_blank"><img className="logo" src="/images/logo.png" alt="logo"/></a>
+                    <a href="https://www.dscommunity.in/" target="_blank"><img className="logo" src={logo} alt="logo"/></a>
                     <input type="search" />
                     <img className="search-icon" src="/images/search.svg" alt="pfp"/>
                     <button className={props.cartColor} onClick={props.nextPage}>{props.cartItemNumber}</button>
-                    <a href="https://github.com/xAdhithiyan" target="_blank"><img className="pfp" src="/images/pfp.jpg" alt="" /></a>
+                    <a href="https://github.com/xAdhithiyan" target="_blank"><img className="pfp" src={pfp} alt="pfp" /></a>
                 </div>
                 <div className="company" >DreamWeave <div className="company-moto" >Embark on a Journey of Fashion, Where Every Outfit Tells Your Story</div></div>
 
